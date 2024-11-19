5cde50c1f9a51c4a37cd062f2990323cb3445e53
ShayanAnsariMm1 <shayanansari074@gmail.com>
2024-11-19 13:34:44 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <!-- CSS LINK -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/index.css">
+        <style>
+
+        </style>
+    </head>
+
+    <body class="">
+        <header class="header">
+            <a class="logo" href="index.php"><img src="logo/LOGO.png" class="logo-img" alt="" width="150px" height="80px"></a>
+            <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+            <ul class="menu">
+              <li><a href="index.php" class="link link-theme link-arrow">HOME</a></li>
+              <li><a href="players.php" class="link link-theme link-arrow">PLAYERS</a></li>
+              <li><a href="about.php" class="link link-theme link-arrow">ABOUT US</a></li>
+              <li><a href="contact.php" class="link link-theme link-arrow">CONTACT</a></li>
+            </ul>
+          </header>
+          
+        <main>
+            <div class="carousel">
+                <div class="list">
+
+                    <div class="item" style="background-image: url(image/ROBERT1.png);">
+                        <div class="content">
+                            <div class="title">ROBERT</div>
+                            <div class="name">LEWAN-<br>DOWSKI</div>
+                            <div class="des">Robert Lewandowski, a prolific Polish striker, is renowned for his clinical finishing, remarkable consistency, and ability to score goals from almost any position, establishing him as one of the greatest goal-scorers in football history.</div>
+                            
+                        </div>
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/RONALDO1.png);">
+                        
+                        <div class="content">
+                            <div class="title">CRISTIANO</div>
+                            <div class="name">RONALDO</div>
+                            <div class="des">Cristiano Ronaldo, one of the greatest footballers of all time, is known for his incredible goal-scoring ability, unmatched work ethic, and a career filled with numerous records and accolades across Europe’s top clubs.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/MESSI1.png);">
+        
+                        <div class="content">
+                            <div class="title">LIONEL</div>
+                            <div class="name">MESSI</div>
+                            <div class="des">Lionel Messi, often hailed as the greatest footballer in history, is celebrated for his extraordinary dribbling, vision, and an unparalleled ability to create and score goals, earning him numerous records and titles throughout his illustrious career.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/MBAPPE1\ .png);">
+                        
+                        <div class="content">
+                            <div class="title">KYLIAN </div>
+                            <div class="name">MBAPPE</div>
+                            <div class="des">Kylian Mbappé, a generational talent, is renowned for his blistering speed, exceptional skill, and goal-scoring prowess, making him one of the brightest stars in world football at a young age.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/NEYMAR1.png);">
+                        
+                        <div class="content">
+                            <div class="title">NEYMAR</div>
+                            <div class="name">JUNIOR</div>
+                            <div class="des">Neymar Jr., a Brazilian football sensation, is celebrated for his dazzling dribbling, creativity, and flair, making him one of the most exciting and influential players of his generation.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/RIYAD1.png);">
+                        
+                        <div class="content">
+                            <div class="title">RIYAD</div>
+                            <div class="name">MAHREZ</div>
+                            <div class="des">Riyad Mahrez, a skillful Algerian winger, is known for his silky dribbling, precise left foot, and crucial contributions in helping his teams secure major titles, including Premier League triumphs.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/ERLING1.png);">
+                        
+                        <div class="content">
+                            <div class="title">ERLING</div>
+                            <div class="name">HAALAND</div>
+                            <div class="des">Erling Haaland, a Norwegian striker, is celebrated for his exceptional goal-scoring ability, lightning-fast pace, and physical dominance, making him one of the most feared forwards in world football.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/MANE1.png);">
+                        
+                        <div class="content">
+                            <div class="title">SADIO</div>
+                            <div class="name">MANE</div>
+                            <div class="des">Sadio Mané, a dynamic Senegalese forward, is known for his explosive speed, relentless work rate, and ability to deliver in crucial moments, making him a key player in both club and international football.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/VIRGIL1.png);">
+                        
+                        <div class="content">
+                            <div class="title">VIRGIL</div>
+                            <div class="name">VAN DIJK</div>
+                            <div class="des">Virgil van Dijk, a commanding Dutch center-back, is renowned for his defensive prowess, aerial dominance, and leadership on the pitch, solidifying his reputation as one of the best defenders in the world.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                    <div class="item" style="background-image: url(image/KEVIN1.png);">
+                        
+                        <div class="content">
+                            <div class="title">KEVIN</div>
+                            <div class="name">DE BRUYNE</div>
+                            <div class="des">Kevin De Bruyne, a Belgian midfield maestro, is widely acclaimed for his exceptional vision, precise passing, and ability to dictate the tempo of a game, making him one of the most complete playmakers in football.</div>
+                            
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+                <!--next prev button-->
+                <div class="arrows">
+                    <button class="prev"><-</button>
+                    <button class="next">-></button>
+                </div>
+        
+        
+                <!-- time running -->
+                <div class="timeRunning"></div>
+            </div>
+
+            <section class="section1 container-fluid text-center">
+                <br><br><br>
+                <h1>Footballs</h1>
+                <br><br><br>
+              </section>
+
+        <section class="section2">
+            <div class="container">
+
+                <div class="i-box">
+
+                <div id="box1" class="box">
+                    <span></span>
+                    
+                </div>
+                <div id="box2" class="box">
+                    <span></span>
+                    
+                </div>
+                <div id="box3" class="box">
+                    <span></span>
+                    
+                </div>
+                <div id="box4" class="box">
+                    <span></span>
+                    
+                </div>
+                <div id="box5" class="box">
+                    <span></span>
+                    
+                </div>
+                <div id="box6" class="box">
+                    <span></span>
+                    
+                </div>
+
+                </div>
+            </div>
+        </section>
+
+        <br><br><br>
+
+        </main>
+        <footer>
+        <div class="footer">
+           <div class="container">
+              <div class="row">
+                 <div class="col-md-8 offset-md-2">
+                    <h3>Follow Us</h3>
+                    <ul class="social_icon text_align_center">
+                       <li> <a href="Javascript:void(0)"><i class="fa fa-facebook-f"></i></a></li>
+                       <li> <a href="Javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+                       <li> <a href="Javascript:void(0)"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                       <li> <a href="Javascript:void(0)"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                       <li> <a href="Javascript:void(0)"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                    </ul>
+                    <div class="conta text_align_center">
+                       <ul>
+                          <li> <a class="fa fa-phone" href="Javascript:void(0)"> Call +92-315-1413927</a>
+                          </li>
+                          <li> <a class="fa fa-envelope" href="Javascript:void(0)"> footballers@player.edu.pk</a>
+                          </li>
+                       </ul>
+                    </div>
+                 </div>
+                 <div class="col-md-8 offset-md-2">
+                    <div class="menu_bottom text_align_center">
+                       <ul>
+                          <li><a href="index.php">HOME</a></li>
+                          <li><a href="players.php">PLAYERS</a></li>
+                          <li><a href="about.php">ABOUT US</a></li>
+                          <li><a href="contact.php">CONTACT</a></li>
+                       </ul>
+                    </div>
+                 </div>
+              </div>
+           </div>
+           <div class="copyright text_align_center">
+              <div class="container">
+                 <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                       <p>© 2024 All Rights Reserved. <a href="index.php"> Footballers</a></p>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </footer>
+    </body>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="js/try.js"></script>
+</html>
